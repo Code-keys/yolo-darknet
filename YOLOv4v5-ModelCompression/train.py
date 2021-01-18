346df17e29781135f042e5fb8d14e61b4acd5784
codekey <cx574251@gmail.com>
2021-01-18 16:41:44 +0800

    Update train.py

--- YOLOv4v5-ModelCompression/train.py
+++ YOLOv4v5-ModelCompression/train.py
@@ -31,7 +31,7 @@ hyp = {'giou': 3.54,  # giou loss gain
        'obj': 64.3,  # obj loss gain (*=img_size/320 if img_size != 320)
        'obj_pw': 1.0,  # obj BCELoss positive_weight
        'iou_t': 0.20,  # iou training threshold
-       'lr0': 0.01,  # initial learning rate (SGD=5E-3, Adam=5E-4)
+       'lr0': 0.001,  # initial learning rate (SGD=5E-3, Adam=5E-4)
        'lrf': 0.0005,  # final learning rate (with cos scheduler)
        'momentum': 0.937,  # SGD momentum
        'weight_decay': 0.0005,  # optimizer weight decay
